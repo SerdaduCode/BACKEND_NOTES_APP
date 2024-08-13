84e0aaab7d112a77283b1f988dd2fce8b9b0995d
dhyogap@gmail.com <dhyogap@gmail.com>
2024-08-13 20:58:45 +0700

    setup database

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
+import { prisma } from "./config/database.js"
 import NotesController from "./controller/notes.js";
 
 const app = express();
 const port = 3000;
 
-const notesController = new NotesController();
-routes(app, notesController);
+prisma ? console.log("Connection Success") : console.log("Connection Failed")
 
 app.listen(port, () => {
   console.log(`Notes app listening on port ${port}`);
---
+++ libs/migrations/20240813135112_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT,
+
+    CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
+);
