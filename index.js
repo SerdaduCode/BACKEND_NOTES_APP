0faf9af671e07e64288c4da7ceeeacc70086a00a
Syaid <syaidaja23@gmail.com>
2024-08-19 16:46:55 +0700

    feature: CORS

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import express, { json } from "express";
-import database from "./config/database.js"
+import database from "./config/database.js";
 import apiRouter from "./routes/index.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(json())
+app.use(json());
+app.use((_, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-type, Authorization, Cache-control"
+  );
+  next();
+});
 
 try {
   await database.$connect();
@@ -17,7 +29,7 @@ app.get("/", (req, res) => {
   res.send("SerdaduNote Backend");
 });
 
-apiRouter(app)
+apiRouter(app);
 
 app.listen(port, () => {
   console.log(`Notes app listening on port ${port}`);
